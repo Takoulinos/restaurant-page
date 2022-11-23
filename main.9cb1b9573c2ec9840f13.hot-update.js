dbab7f54f2abaceaff06a100b6cca06179e2c107
Takoulinos <theo100theo@yahoo.gr>
2022-11-23 21:37:40 +0200

    project finished

---
+++ main.9cb1b9573c2ec9840f13.hot-update.js
@@ -0,0 +1,22 @@
+self["webpackHotUpdaterestaurant_page"]("main",{
+
+/***/ "./src/loadContact.js":
+/*!****************************!*\
+  !*** ./src/loadContact.js ***!
+  \****************************/
+/***/ (() => {
+
+throw new Error("Module parse failed: Export 'loadContact' is not defined (12:9)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n| }\n| \n> export { loadContact }");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("91b65f4a019ce7d68fee")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi45Y2IxYjk1NzNjMmVjOTg0MGYxMy5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7VUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiOTFiNjVmNGEwMTljZTdkNjhmZWVcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
