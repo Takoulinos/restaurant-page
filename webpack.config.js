4468657c59639e2595b9fb7dfb10324d000d36a5
Takoulinos <theo100theo@yahoo.gr>
2022-11-23 18:14:58 +0200

    added a few plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
+  devServer: {
+    port: 9000,
+    static: {
+        directory: path.resolve(__dirname, './dist'),
+    },
+    devMiddleware: {
+        index: 'index.html',
+        writeToDisk: true
+    }
+  },
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: ''
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        filename: 'index.html'
+    })
+  ]
 };
